2cbf65db6ffe65dec4ab4487dda9e83c75faebe1
VanisaIndriyani <vanisaindriyani30@gmail.com>
2026-02-06 21:40:00 +0700

    Rename script.js to main-script.js to bust browser cache

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -145,7 +145,7 @@
     @includeIf('components.modal-success')
 
     {{-- JS --}}
-    <script src="{{ asset('js/script.js') }}?v={{ time() }}"></script>
+    <script src="{{ asset('js/main-script.js') }}?v={{ time() }}"></script>
 
     @stack('scripts')
 
